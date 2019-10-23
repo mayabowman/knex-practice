38b0e69cc1d9e1d18428a6d718c1c5b58e60d446
Maya Bowman <mayashady1@gmail.com>
2019-10-23 09:28:20 -0400

    working through knex practice

--- src/practice.js
+++ src/practice.js
@@ -1,1 +1,13 @@
-console.log('hello, practice')
+const knex = require('knex')
+
+const knexInstance = knex({
+  client: 'pg',
+  connection: process.env.DB_URL,
+})
+
+const q2 = knexInstance.from('amazong_products').select('*')
+  .then(result => {
+    console.log(result)
+  })
+
+console.log('knex and driver installed properly')
