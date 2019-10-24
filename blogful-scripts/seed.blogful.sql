8580b53a2cf783a1dae70db0582205261064ee44
Maya Bowman <mayashady1@gmail.com>
2019-10-24 09:33:05 -0400

    started blogful exercise

---
+++ blogful-scripts/seed.blogful.sql
@@ -0,0 +1,22 @@
+INSERT INTO blogful_articles (title, content, date_published)
+  VALUES
+    ('Dummy Article One', 'Dummy for Article One', now() - '1 days'::INTERVAL),
+    ('Dummy Article Two', 'Dummy for Article Two', now() - '2 days'::INTERVAL),
+    ('Dummy Article Three', 'Dummy for Article Three', now() - '3 days'::INTERVAL),
+    ('Dummy Article Four', 'Dummy for Article Four', now() - '4 days'::INTERVAL),
+    ('Dummy Article Five', 'Dummy for Article Five', now() - '5 days'::INTERVAL),
+    ('Dummy Article Six', 'Dummy for Article Six', now() - '6 days'::INTERVAL),
+    ('Dummy Article Seven', 'Dummy for Article Seven', now() - '7 days'::INTERVAL),
+    ('Dummy Article Eight', 'Dummy for Article Eight', now() - '8 days'::INTERVAL),
+    ('Dummy Article Nine', 'Dummy for Article Nine', now() - '9 days'::INTERVAL),
+    ('Dummy Article Ten', 'Dummy for Article Ten', now() - '10 days'::INTERVAL),
+    ('Dummy Article Eleven', 'Dummy for Article Eleven', now() - '11 days'::INTERVAL),
+    ('Dummy Article Twelve', 'Dummy for Article Twelve', now() - '12 days'::INTERVAL),
+    ('Dummy Article Thirteen', 'Dummy for Article Thirteen', now() - '13 days'::INTERVAL),
+    ('Dummy Article Fourteen', 'Dummy for Article Fourteen', now() - '14 days'::INTERVAL),
+    ('Dummy Article Fifteen', 'Dummy for Article Fifteen', now() - '15 days'::INTERVAL),
+    ('Dummy Article Sixteen', 'Dummy for Article Sixteen', now() - '16 days'::INTERVAL),
+    ('Dummy Article Seventeen', 'Dummy for Article Seventeen', now() - '17 days'::INTERVAL),
+    ('Dummy Article Eighteen', 'Dummy for Article Eighteen', now() - '18 days'::INTERVAL),
+    ('Dummy Article Nineteen', 'Dummy for Article Nineteen', now() - '19 days'::INTERVAL),
+    ('Dummy Article Twenty', 'Dummy for Article Twenty', now() - '20 days'::INTERVAL)
