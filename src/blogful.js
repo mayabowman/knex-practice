731a70fe63c1721540ebe0749e78362aff5bd675
Maya Bowman <mayashady1@gmail.com>
2019-10-25 08:25:21 -0400

    added testing

--- src/blogful.js
+++ src/blogful.js
@@ -7,4 +7,26 @@ const knexInstance = knex({
   connection: process.env.DB_URL,
 })
 
+ArticlesService.getAllArticles(knexInstance)
+  .then(articles => console.log(articles))
+  .then(() => 
+    ArticlesService.insertArticle(knexInstance, {
+      title: 'New title',
+      content: 'New content',
+      date_published: new Date(),
+    })
+  )
+  .then(newArticle => {
+    console.log(newArticle)
+    return ArticlesService.updateArticle(
+      knexInstance,
+      newArticle.id,
+      { title: 'Updated title' }
+    ).then(() => ArticlesService.getById(knexInstance, newArticle.id))
+  })
+  .then(article => {
+    console.log(article)
+    return ArticlesService.deleteArticle(knexInstance, article.id)
+  })
+
 console.log(ArticlesService.getAllArticles())
